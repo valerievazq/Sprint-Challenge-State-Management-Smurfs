4444f13e0aec370fd6012eda28019f7ae484899a
Valerie Vazquez <vnvazquez68@gmail.com>
2020-06-27 13:28:01 -0600

    minor changes

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -7,19 +7,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className='container'>
+        <div>
           <Jumbotron fluid style = {{padding: '5px'}}>
-            <Container fluid>
+            <Container className='container' fluid>
               <h1 className="display-3">Welcome to the Smurf's Village!</h1>
               <p className="lead">
                 We do not have enough Smurfs to sustain our village. Fill out the form bellow to add some new smurfs to our town!
               </p>
             </Container>
           </Jumbotron>
-          <Smurfs />
+          <div className='residents'> <h1>Smurf Residents</h1>
+          <Smurfs /></div>
+         
         </div>
         <div className="smurfs-form">
-          <h1>Add New Smurfs To Smurf Village</h1>
+          <h1>New Smurf Application</h1>
           <SmurfsForm />
         </div>
       </div>
